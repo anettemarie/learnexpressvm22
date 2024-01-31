a39bf3ad0cc2436d61bd41001cfb7be5148fde89
Anette Marie Põldsalu <anettemarie.poldsalu@tptlive.ee>
2024-01-31 14:15:55 +0200

    jee

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const nunjucks = require('nunjucks')
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({
+  extended:true
+}))
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
@@ -24,6 +28,15 @@ app.get('/page2', (req, res) => {
     res.render('form.njk', req.query);
   });
 
+  app.get('/circle', (req, res) => {
+    res.render('circle.njk', req.query);
+  });
+  app.post('/circle', (req, res) => {
+    let area = Math.PI * req.body.radius * req.body.radius;
+    let circumference = 2 * Math.PI * req.body.radius ;
+    let volume = 4/3 * Math.PI * req.body.radius* req.body.radius* req.body.radius ;
+    res.render("circleanswer.njk", {r: req.body.radius, a: area, c: circumference, v:volume });
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
