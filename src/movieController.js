731f8ef4257f4b9c0f81e81c706da954920c0a45
Anette Marie Põldsalu <anettemarie.poldsalu@tptlive.ee>
2024-02-07 13:18:58 +0200

    juhu

--- src/movieController.js
+++ src/movieController.js
@@ -1,8 +1,12 @@
 const express = require ("express");
 const router = express.Router();
+const fs = require("fs");
 
 router.get("/", (req, res)=> {
-   res.send("movies")
+   //fs.appendFileSync("test.txt", "hello");
+   let text = fs.readFileSync("test.text", "utf8")
+   console.log(text)
+   res.send(text.toString)
 } )
 
 module.exports = router;
