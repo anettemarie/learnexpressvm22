6ca9f14e95ca174c076ba82923d2b0ab1e401d0a
anettemarie <146168771+anettemarie@users.noreply.github.com>
2024-04-03 12:38:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# learnexpressvm22
+# vm22express
+learn express
 
-Terminalis node "lehe nimi mida tahad tööle saada"
-Node server.js
+vsc - console.log('hello terminal') -> terminal
+-> terminal - node faili.nimi
+hello terminal
 
-code . - vscode
-npm install express - annab meile package.json-id
 
-.gitignore - failid mida tahame, et poleks nö näha
+terminal - npm install express - andis package-lock.json ja node_modules
 
-learnexpressvm22@1.0.0 start - $ npm run start
+
+vsc - fail main kataloogis ''.gitignore'' - git, pls igno seda faili
+faili sisse kirjuta failid (node_modules), mida tahad ignoda
+
+
+et ei peaks igakord crl+c uuendamiseks - terminal .. ''npx nodemon''
+package.jsonis  "main": "server.js", mitte index.html, et töötaks
+
+NPM START
+
+
--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-console.log('hello terminal');
-
 const express = require('express');
-const nunjucks = require('nunjucks')
+const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-const session =require('express-session');
+const session = require('express-session');
 app.use(session({
   secret: 'secret'
 }));
 
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(express.urlencoded({extended:true}))
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -28,30 +24,37 @@ app.get('/', (req, res) => {
 
 app.get('/page2', (req, res) => {
     res.render('page2.njk');
-  });
+});
 
-  app.get('/form', (req, res) => {
-    console.log(req.query);
-    res.render('form.njk', req.query);
-  });
+app.get('/form', (req, res) => {
+  console.log(req.query);
+  res.render('form.njk', req.query);
+});
 
-  app.get('/circle', (req, res) => {
-    res.render('circle.njk', req.query);
-  });
-  app.post('/circle', (req, res) => {
-    let area = Math.PI * req.body.radius * req.body.radius;
-    let circumference = 2 * Math.PI * req.body.radius ;
-    let volume = 4/3 * Math.PI * req.body.radius* req.body.radius* req.body.radius ;
-    res.render("circleanswer.njk", {r: req.body.radius, a: area, c: circumference, v:volume });
-  });
+app.get('/circle', (req, res) => {
+  res.render('circle.njk', req.query);
+});
 
-const movieController = require("./src/movieController.js")
-app.use("/movies", movieController)
+app.post('/circle', (req, res) => {
+  let area = Math.PI * req.body.radius * req.body.radius;
+  let circumference = 2 * Math.PI * req.body.radius;
+  let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
+  res.render("circleAnswer.njk", {r: req.body.radius, a: area, c: circumference, v: volume });
+});
+
+const movieController = require('./src/movieController.js');
+app.use('/movies', movieController);
 
 app.get('/cookie', (req, res) => {
-  res.cookie('mycookie', 'cool cookie', {maxAge: 1000*60*60*24*30});
-res.send(req.cookies);
+  res.cookie('mycookie', 'kõrsikud', {maxAge: 1000*60*60*24*30});
+  if(!req.session.secretValue) {
+    req.session.secretValue = new Date();   
+  }
+  res.send(req.session);
 });
+
+const authController = require('./src/authController.js');
+app.use(authController);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
