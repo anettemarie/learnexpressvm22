58412218f4afcb5927933644b1586d2a72fae3f0
Anette Marie Põldsalu <anettemarie.poldsalu@tptlive.ee>
2024-04-17 13:27:17 +0300

    df

--- src/models/User.js
+++ src/models/User.js
@@ -1,28 +1,27 @@
-const {Sequelize, QueryTypes, DataTypes} = require('sequelize');
+const {Sequelize, QueryTypes, DataTypes } = require('sequelize');
 let sequelize = new Sequelize('sqlite:db.sqlite');
-
+const Movie = require('./Movie.js');
 const User = sequelize.define('User', {
     id: {
-       type: DataTypes.INTEGER,
-       autoIncrement: true,
-       primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
     },
     name: {
-       type: DataTypes.STRING,
-       allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     email: {
-       type: DataTypes.STRING,
-       allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     password: {
-       type: DataTypes.STRING,
-       allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     }
- }, {tableName: 'users', timestamps:false});
+}, {tableName: 'users', timestamps:false});
+User.associations = () => {
+    User.hasMany(Movie);
+};
 
- User.hasMany(Movie, {
-   foreignKey: 'user_id'
- });
-
- module.exports = User;
+module.exports = User;
